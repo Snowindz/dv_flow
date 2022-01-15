f39582d201c0386aab9f22060d13bc241cfe0d6b
Snowind <snowind83@163.com>
2022-01-15 12:48:16 +0800

    Create file_tree.sh

---
+++ file_tree.sh
@@ -0,0 +1,97 @@
+File Tree structure
+
+./VERSION           # Version number file (like 1.0.0.0)
+./doc               # documentations
+
+./bin               # Soft links to scripts executables, or new scripts
+
+./flows             # main flow scripts (.mk for each target)
+./flows/help*       # help*.txt
+./flows/helpers     # *mk
+./flows/bar.mk      # top level MK for Build & Run   
+# sub dir
+  ./include/common.mk   # call gmsl, define common mk variables like SHELL/VERBOSITY/QUIET ...
+  ./include/gmsl        # wrapper of __gmsl by using ifndef/define/endif to avoid of duplicated call
+  ./include/__gmsl      # GNU Make Standard Lib, version := 1.1.5
+
+  ./utility.mk          # define UTILITY_TARGETS
+  ./query.mk            # define query_info function
+
+  ./dut.mk
+  dut_lib.mk
+  hdl.mk
+  
+  hvl.mk
+  hvl_link.mk
+  hvl_link_indut.mk
+  
+  tb.mk
+  th.mk
+  tb_doc.mk
+  tb_libs.mk
+  tb_lint.mk
+  
+  abv.mk
+  abv_libs.mk
+  
+  elab.mk
+  elab_dut.mk
+  
+  sim.mk
+  sim_top.mk
+  
+  aldec.mk
+  app.mk
+  common_libs.mk
+  compatibility.mk
+  
+  coverage.mk
+  covercheck.mk
+  
+  debug.mk
+  design_dump_info.mk
+  enum.mk
+  fsim.mk
+  
+  hlverif.mk
+  hlverif_legacy_vars_mapping.mk
+  ice.mk
+  jasper.mk
+  prove.mk
+  
+  macro.mk
+  misc.mk
+  pwrest.mk
+  sandbox_ide.mk
+  synopsys.mk
+  template.mk
+  test.mk
+  tools.mk
+  unr.mk
+  upf.mk
+  vc_formal.mk
+  vcs_unr.mk
+  vip_stats.mk
+  
+  
+  
+  
+
+                 
+                 
+./template
+./wrappers  
+
+./scripts           # Wrapper/top level scripts based on libs
+
+./lib               # libs for scripts, or .so/.a libs
+./lib/linux
+./lib/solaris
+
+./perl_lib          # standalone/shared/utils perl libs for lib/scripts
+./python_lib        # standalone/shared/utils py libs for lib/scripts
+
+
+
+
+
