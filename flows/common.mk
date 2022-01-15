8bd9bf7b64cfb19990b3b75b5260d80bf83dc2d9
Snowind <snowind83@163.com>
2022-01-15 21:40:30 +0800

    formatted

--- flows/common.mk
+++ flows/common.mk
@@ -20,9 +20,16 @@ __gmsl_tr1 :=
 __gmsl_tr2 :=
 __gmsl_tr3 :=
 endif
+
+# Function: seq <a str to compare w/> <this str>
+# Return: return $(true) if two strs are identical
 seq = $(__gmsl_tr2)$(if $(subst x$1,,x$2)$(subst x$2,,x$1),$(false),$(true))
+
 uc = $(shell echo $1 | tr a-z A-Z)
 lc = $(shell echo $1 | tr A-Z a-z)
+
+# Function: uniq <list to be remove duplications>
+# Return: list which rmv the duplications
 uniq = $(strip $(__gmsl_tr1) $(if $1,$(firstword $1) \
        $(call uniq,$(filter-out $(firstword $1),$1))))
 
