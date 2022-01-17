7362028cc2245cb97e4321b5f0cb709a480b4a67
Snowind <snowind83@163.com>
2022-01-17 16:50:35 +0800

    Create file_tree_rgr.sh

---
+++ doc/file_tree_rgr.sh
@@ -0,0 +1,21 @@
+
+./doc/
+  ./helppages/ (*.pod)
+
+./etc/    # fail/pass pattern (*.qre)
+  fvcad.qre
+    
+  it.qre
+  qwebstats_regresion_url_map.yml
+  
+./include/virtio.qri
+
+./lib/
+  ./linux/      # libdb-4.3.so/la/a
+  ./sun/
+  
+./perlib/
+./sripts/
+./utils/
+./FlowTracer/
+
