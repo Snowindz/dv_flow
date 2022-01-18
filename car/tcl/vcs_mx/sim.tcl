d3389418990f34651374bf174f3fe052984a8773
Snowind <snowind83@163.com>
2022-01-18 16:15:08 +0800

    Update sim.tcl

--- car/tcl/vcs_mx/sim.tcl
+++ car/tcl/vcs_mx/sim.tcl
@@ -1,7 +1,61 @@
-puts "# Source vcs_mx/sim.do"
+set this_tcl_file "sim.tcl"
+puts "# Source vcs_mx/$this_tcl_file"
 
-set env(DV_RGR_ROOT) $rgr_vars(DV_RGR_ROOT)
-source $rgr_vars(DV_CAR_ROOT)/tcl/$rgr_vars(SIM)/on_start.tcl
-source $rgr_vars(DV_CAR_ROOT)/tcl/$rgr_vars(SIM)/run.tcl
-source $rgr_vars(DV_CAR_ROOT)/tcl/$rgr_vars(SIM)/on_exit.tcl
-source $rgr_vars(DV_CAR_ROOT)/tcl/$rgr_vars(SIM)/quit.tcl
+#set env(DV_RGR_ROOT) $rgr_vars(DV_RGR_ROOT)
+##############################
+# on_start
+##############################
+# common.tcl
+
+# fsdb/vpd/vcd/evcd dump setup
+# fsdb.tcl
+# vpd.tcl
+# vcd.tcl
+# evcd.tcl
+
+config endofsim noexit
+set user_cmds [list]
+if { [info exists env(RGR_SIMV_ON_START_TCL_CMDS) } {
+  set user_cmds [ split $env(RGR_SIMV_ON_START_TCL_CMDS) {;} ]
+  puts "[$this_tcl_file] Info: found user defined cmd files from env RGR_SIMV_ON_START_TCL_CMDS"
+}
+foreach cmd_file $user_cmds {
+  eval $cmd_file
+}
+
+
+
+##############################
+# run
+##############################
+# - common.tcl
+# - dump_times.cl
+# - sar.tcl
+set rgr_sim_run_time ""
+if { [info exist rgr_vars(__SIM_RUN_TIME) ] } {
+set rgr_sim_run_time $rgr_vars(__SIM_RUN_TIME)
+}
+run $rgr_sim_run_time
+
+##############################
+# on_exit.tcl
+##############################
+# - common.tcl
+# last save
+# dump flush/off
+
+set user_cmds [list]
+if { [info exists env(RGR_SIMV_ON_EXIT_TCL_CMDS) } {
+  set user_cmds [ split $env(RGR_SIMV_ON_EXIT_TCL_CMDS) {;} ]
+  puts "[$this_tcl_file] Info: found user defined cmd files from env RGR_SIMV_ON_EXIT_TCL_CMDS"
+}
+foreach cmd_file $user_cmds {
+  eval $cmd_file
+}
+
+
+##############################
+# quit.tcl
+##############################
+quit
+
