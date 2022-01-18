30a8bc74dabc4355c6e0cbcd0ae7195b9325d4d1
Snowind <snowind83@163.com>
2022-01-18 16:38:26 +0800

    Update cmn_proc.tcl

--- car/tcl/cmn/cmn_proc.tcl
+++ car/tcl/cmn/cmn_proc.tcl
@@ -9,7 +9,7 @@ set __cmn_proc__tcl 1
 # Run cmd and check result
 # - tcl_file: the tcl file called
 # - cmd: cmd executed inside tcl_file
-proc run_cmd { tcl_file cmd } {
+proc run_cmd_in_tcl_file { tcl_file cmd } {
   set run_cmd_ts [clock format [clock seconds] -format "%a %b %d %H:%M:%S %Z %Y"]
   puts "#\[$run_cmd_ts] $tcl_file: $cmd"  
   if { [ catch { eval $cmd } msg ] > 0 } {
