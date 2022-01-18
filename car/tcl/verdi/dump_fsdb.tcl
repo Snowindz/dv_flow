437c05d1040ce4c553d98f8ead47dc2e54785aa6
Snowind <snowind83@163.com>
2022-01-18 18:35:52 +0800

    Update dump_fsdb.tcl

--- car/tcl/verdi/dump_fsdb.tcl
+++ car/tcl/verdi/dump_fsdb.tcl
@@ -3,14 +3,32 @@ source $env(DV_CAR_ROOT)/tcl/cmn/cmn_proc.tcl
 puts "# Source vcs_mx/$tcl_file"
 
 set fsdb_dump_on 0
-if { [info exists env(WAVES_FSDB_FILE)] } {
-  if { [info exists env(WAVES_DUMP_ON)] && ($env(WAVES_DUMP_ON) ==1) } {
+# define WAVES_FSDB_FILE
+if { [ info exists rgr_vars(WAVES_FSDB_FILE) ] } {
+  if { [info exists rgr_vars(WAVES_DUMP_ON)] } {
+    set fsdb_dump_on $rgr_vars(WAVES_DUMP_ON)
+  } else {
     set fsdb_dump_on 1
   }
 }
 
-if { $fsdb_dump_on } {
-
-
-
+if { $fsdb_dump_on == 0 } {
+  exit 0
 }
+
+set fsdb_task_call ""
+if { $rgr_vars(SIM) == "ius" } {
+  set fsdb_task_call "call "
+}
+
+set WAVES_FSDB_DUMP_SVA [get_rgr_var_bool WAVSS_FSDB_DUMP_SVA]
+set WAVES_FSDB_DUMP_ALL [get_rgr_var_bool WAVES_FSDB_DUMP_ALL]
+set WAVES_FSDB_DUMP_REG_ONLY [get_rgr_var_bool WAVES_FSDB_DUMP_REG_ONLY]
+set WAVES_FSDB_DUMP_IO_ONLY [get_rgr_var_bool WAVES_FSDB_DUMP_IO_ONLY]
+
+set WAVES_FSDB_DUMP_LST_FILES [get_rgr_var_list WAVES_FSDB_DUMP_LST_FILES]
+set WAVES_FSDB_DUMP_LST_FILES [get_rgr_var_list WAVES_FSDB_DUMP_LST_FILES]
+
+
+
+
