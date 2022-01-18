eea40d889d894214d89bd564ca7e992ba11b696b
Snowind <snowind83@163.com>
2022-01-18 23:00:02 +0800

    Update sim.tcl

--- car/tcl/vcs_mx/sim.tcl
+++ car/tcl/vcs_mx/sim.tcl
@@ -8,7 +8,7 @@ puts "# Source vcs_mx/$tcl_file"
 ##############################
 # fsdb/vpd/vcd/evcd dump setup
 source $env(DV_CAR_ROOT)/tcl/verdi/dump_fsdb.tcl
-source $env(DV_CAR_ROOT)/tcl/$rgr_vars(SIM)/dump_vpd.tcl
+source $env(DV_CAR_ROOT)/tcl/vcs_mx/dump_vpd.tcl
 source $env(DV_CAR_ROOT)/tcl/$rgr_vars(SIM)/dump_vcd.tcl
 source $env(DV_CAR_ROOT)/tcl/$rgr_vars(SIM)/dump_evcd.tcl
 
@@ -17,7 +17,7 @@ run_tcl_cmd $tcl_file "config endofsim noexit"
 
 #set user_cmds [list]
 #if { [info exists env(RGR_SIMV_ON_START_TCL_CMDS) ] } {
-#  set user_cmds [ split $env(RGR_SIMV_ON_START_TCL_CMDS) {;} ]
+#  set user_cmds [ split trim $env(RGR_SIMV_ON_START_TCL_CMDS) {;} ]
 #  puts "[$tcl_file] Info: found user defined cmd files from env RGR_SIMV_ON_START_TCL_CMDS"
 #}
 
@@ -50,9 +50,21 @@ run_tcl_cmd $tcl_file "run $sim_run_time"
 ##############################
 # on_exit.tcl
 ##############################
-# - common.tcl
-# last save
+source $env(DV_CAR_ROOT)/tcl/vcs_mx/save_retain_last.tcl
+
 # dump flush/off
+if { [ info exist vpd_dump_on ] && $vpd_dump_on } {
+    if { [ info exist ENABLE_PA_SIM ] && $ENABLE_PA_SIM } {
+        run_tcl_cmd $tcl_file "dump -power off"
+    }
+}
+if { [ info exist vcd_dump_on ] && $vcd_dump_on } {
+    run_tcl_cmd $tcl_file "call {\$dumpflush}"
+}
+if { [ info exist vpd_dump_on ] && $vpd_dump_on } {
+    run_tcl_cmd $tcl_file "dump -flush"
+}
+
 
 #set user_cmds [list]
 #if { [info exists env(RGR_SIMV_ON_EXIT_TCL_CMDS) ] } {
